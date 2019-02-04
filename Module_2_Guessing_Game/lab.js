a02cf00795755fa260aa461d28106456698237fd
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-04 04:53:05 -0700

    Update

--- Module_2_Guessing_Game/lab.js
+++ Module_2_Guessing_Game/lab.js
@@ -1,17 +1,27 @@
 // program data:
 var number = 4;
 var guess;
+var limit = 5;
+var won = false;
 
+for(i = 1;i <= limit; i++){
+    // prompt user for their guess
+    guess = prompt("Guess a number");
 
-// prompt user for their guess
-guess = prompt("Guess a number");
+    // if correct: let the user know they won
+    if(guess == number){
+        document.write("Correct! You won.");
+        won = true;
+        break;
+    }
 
-// if correct: let the user know they won
-if(guess == number){
-    document.write("Correct! You won.")
-}
+    // if incorrect: let the user know
+    else{
+        alert("Incorrect. Tries remaining: " + (limit - i));
+    }
+};
 
-// if incorrect: let the user know
-else{
-    alert("Incorrect");
-}
+// if the user ran out of tries, let them know the game is over
+if(!(won == false)){
+    document.write("Sorry, you ran out of tries. Game over.");
+};
