9a2c18ee8bc6dd40d6d9af9fe13020f47d5c589b
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-04 07:14:38 -0700

    Update

--- Module_2_Guessing_Game/lab.js
+++ Module_2_Guessing_Game/lab.js
@@ -1,27 +1,40 @@
 // program data:
-var number = 4;
+var number = Math.floor(Math.random() * 100);
 var guess;
 var limit = 5;
-var won = false;
+var won =  false;
+var guesses = [];
 
-for(i = 1;i <= limit; i++){
+// game control loop - iterates for each try
+for (var i = 1; i <= limit; i++){
     // prompt user for their guess
-    guess = prompt("Guess a number");
-
+    do{
+        guess = parseInt(prompt("Guess a number"));
+    } while(isNaN(guess) || isPreviousGuess(guess));
+    
     // if correct: let the user know they won
     if(guess == number){
         document.write("Correct! You won.");
         won = true;
         break;
     }
-
-    // if incorrect: let the user know
+    // if incorrect: add number to guesses array
     else{
-        alert("Incorrect. Tries remaining: " + (limit - i));
+        guesses[i] = guess;
+        alert("incorrect. \nYou have guessed: " + guesses[i] + "\nTries remaining: " + (limit - i));
     }
-};
+}
 
 // if the user ran out of tries, let them know the game is over
-if(!(won == false)){
-    document.write("Sorry, you ran out of tries. Game over.");
-};
+if(!won){
+    document.write("Sorry, you ran out of tries. Game over. The correct number was: " + number);
+}
+
+function isPreviousGuess(){
+    for(var i = 1; i < guesses.length; i++){
+        if(guesses[i] == guess){
+            return true;
+        }
+    }
+    return false;
+}
