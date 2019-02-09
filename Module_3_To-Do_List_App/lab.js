6097354b5434b4a156f32cf5ee5e3fa3e1eadc45
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-09 00:29:46 -0700

    Update

--- Module_3_To-Do_List_App/lab.js
+++ Module_3_To-Do_List_App/lab.js
@@ -1,20 +1,19 @@
 var userInput1 = 0;
+var counter = 0;
 var list1 = [];
 var list2 = [];
 
 while(userInput1 != 2){
     var userInput1 = window.prompt("Add ToDo?\n\n"
                                + "[1] YES\n\n"
-                               + "[2] NO")
+                               + "[2] NO");
     if(userInput1 == 1){
-        var newToDoText = window.prompt("Enter ToDo:\n")
-        var delToDoBut = "Done"
-        list1 += newToDoText.toString()
-        list2 += delTodDoBut
-
+        var newToDoText = window.prompt("Enter ToDo:\n");
+        var delToDoBut = "Done";
+        list1[counter] = newToDoText.toString();
+        list2[counter] = delTodDoBut;
     }
 };
 
-
 document.getElementById("div3").innerHTML = list1;
-document.getElementById("div3").innerHTML = list2;
+document.getElementById("div4").innerHTML = list2;
