2a982022a9b6029f3c59c568d8e603478d07f54e
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-10 05:24:15 -0700

    Update

--- Module_3_To-Do_List_App/lab.js
+++ Module_3_To-Do_List_App/lab.js
@@ -1,18 +1,41 @@
-var button1 = document.getElementById("button1");
-var input1 = document.getElementById("input1");
+// create references to page elements
+var addButton = document.getElementById("add");
+var taskInput = document.getElementById("task");
 var taskList = document.getElementById("taskList");
+var clearButton = document.getElementById("clear");
 
-button1.addEventListener("click", function(){
-    var task = input1.value;
-    
-    if(task.trim()){
-        var newItem = document.createElement("LI");
-        var taskText = document.createTextNode(task);
-        newItem.appendChild(taskText);
-        input1.value = "";
-        taskList.appendChild(newItem);
-    }
-    else{
-        alert("Task cannot be empty");
-    }
+// add new item to task list
+addButton.addEventListener("click", function(){
+  var task = taskInput.value;
+  // Don't add empty string
+  if(task.trim()){
+    // add new task list item
+    var newItem = document.createElement("LI");
+    var taskText = document.createTextNode(task);
+    newItem.appendChild(taskText);
+    // clear text input box
+    taskInput.value = "";
+    // add remove option for new item
+    var removeButton = document.createElement("BUTTON");
+    removeButton.innerHTML = "Done";
+    removeButton.className = "remove";
+    removeButton.addEventListener("click", removeTask);
+    newItem.appendChild(removeButton);
+    taskList.appendChild(newItem);
+  }
+  else{
+    alert("Task cannot be empty");
+  }
 });
+
+// clear entire list
+clearButton.addEventListener("click", function(){
+  taskList.innerHTML = "";
+});
+
+// remove a task from the list
+function removeTask(e){
+  // get the parent list item to remove
+  var taskItem = e.target.parentElement;
+  taskList.removeChild(taskItem); 
+}
