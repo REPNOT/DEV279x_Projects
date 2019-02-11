8617034edcaaec6c5c2642aac21ae9b5eea84f97
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-10 21:52:22 -0700

    Update

---
+++ Module_4_Trivia_App/test.js
@@ -0,0 +1,25 @@
+var newQuestFull = $("#enterQuestion");
+var newQuestChoice1 = $("#enterAnswer1");
+var newQuestChoice2 = $("#enterAnswer2");
+var newQuestChoice3 = $("#enterAnswer3");
+var newQuestChoice4 = $("#enterAnswer4");
+
+newQuestFull.on("click", function(){
+    newQuestFull.attr("value", "")
+});
+
+newQuestChoice1.on("click", function(){
+    newQuestChoice1.attr("value", "")
+});
+
+newQuestChoice2.on("click", function(){
+    newQuestChoice2.attr("value", "")
+});
+
+newQuestChoice2.on("click", function(){
+    newQuestChoice2.attr("value", "")
+});
+
+newQuestChoice2.on("click", function(){
+    newQuestChoice2.attr("value", "")
+});
