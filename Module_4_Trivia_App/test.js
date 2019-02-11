3fb9d3fc25913c495f6b62c9f2a0692706392b51
REPNOT <38998364+REPNOT@users.noreply.github.com>
2019-02-10 23:17:37 -0700

    Update

--- Module_4_Trivia_App/test.js
+++ Module_4_Trivia_App/test.js
@@ -3,6 +3,13 @@ var newQuestChoice1 = $("#enterAnswer1");
 var newQuestChoice2 = $("#enterAnswer2");
 var newQuestChoice3 = $("#enterAnswer3");
 var newQuestChoice4 = $("#enterAnswer4");
+var submitQuestion = $("#submit2");
+var dropDownAnswer = $("#correctAnswer");
+var dropDownOption1 = $("#option1");
+var dropDownOption2 = $("#option2");
+var dropDownOption3 = $("#option3");
+var dropDownOption4 = $("#option4");
+var newQuestForm = $("#newQuestForm");
 
 newQuestFull.on("click", function(){
     newQuestFull.attr("value", "")
@@ -16,10 +23,15 @@ newQuestChoice2.on("click", function(){
     newQuestChoice2.attr("value", "")
 });
 
-newQuestChoice2.on("click", function(){
-    newQuestChoice2.attr("value", "")
+newQuestChoice3.on("click", function(){
+    newQuestChoice3.attr("value", "")
 });
 
-newQuestChoice2.on("click", function(){
-    newQuestChoice2.attr("value", "")
+newQuestChoice4.on("click", function(){
+    newQuestChoice4.attr("value", "")
 });
+
+function test1(){
+    var newLocal = newQuestForm.correctAnswer.value;
+    document.write(newLocal);
+};
